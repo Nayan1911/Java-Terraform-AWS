80dc0a3077f65e19120e6ddf3b032fba26df57de
Nayan1911 <sharmanayan39@gmail.com>
2024-05-17 16:22:38 +0530

    Terraform to EKS

--- main.tf
+++ main.tf
@@ -108,3 +108,8 @@ resource "kubernetes_service" "example_service" {
   }
 }
 
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b"]  # Update with your desired availability zones
+}
+
